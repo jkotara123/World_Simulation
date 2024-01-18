744f81f6bcb7159c561b7242e265df0d2c2fb582
Jakub Kotara <jakubkotara@student.agh.edu.pl>
2024-01-18 20:01:16 +0100

    final commit :)

--- proj_lab/src/main/java/agh/ics/oop/SimulationApp.java
+++ proj_lab/src/main/java/agh/ics/oop/SimulationApp.java
@@ -1,7 +1,6 @@
 package agh.ics.oop;
 
 import agh.ics.oop.presenter.ConfigurationPresenter;
-import agh.ics.oop.presenter.SimulationPresenter;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
--- proj_lab/src/main/java/agh/ics/oop/model/observers/MapChangeListener.java
+++ proj_lab/src/main/java/agh/ics/oop/model/observers/MapChangeListener.java
@@ -1,7 +1,6 @@
 package agh.ics.oop.model.observers;
 
 import agh.ics.oop.Simulation;
-import agh.ics.oop.model.maps.WorldMap;
 
 public interface MapChangeListener {
     void mapChanged(Simulation simulation, String message);
